ebc893bab4638f30ad71b51c0352bf290c298354
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2022-10-06 04:20:40 +0300

    Crafting menu now creates category buttons automatically from RecipeCategory enum

---
+++ Assets/Scripts/Crafting/CraftingCategoryButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CraftingCategoryButton : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI categoryNameText;
+
+    public void SetupButton(string name)
+    {
+        categoryNameText.text = name;
+    }
+
+}
--- Assets/Scripts/Crafting/CraftingSystemUI.cs
+++ Assets/Scripts/Crafting/CraftingSystemUI.cs
@@ -1,5 +1,24 @@
+using System;
 using UnityEngine;
 
 public class CraftingSystemUI : MonoBehaviour
 {
+
+    [SerializeField] private GameObject categoryButton;
+    [SerializeField] private Transform categoryButtonParent;
+
+    private void Start()
+    {
+        var amount = Enum.GetNames(typeof(RecipeCategory)).Length;
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject button = Instantiate(categoryButton, categoryButtonParent);
+            if (button.TryGetComponent(out CraftingCategoryButton category))
+            {
+                category.SetupButton(((RecipeCategory)i).ToString());
+            }
+        }
+
+    }
+
 }
